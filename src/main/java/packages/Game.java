0f6ca448eaa3cbcd81eaef28e7892f75bcec72da
RenegadeB5 <rocketmanB5@gmail.com>
2021-12-06 05:05:41 -0500

    update

--- src/main/java/packages/Game.java
+++ src/main/java/packages/Game.java
@@ -215,6 +215,12 @@ public class Game {
                 .filter(c -> c.position() == this.getPosition(id) && c.color().equals(card[0]) && c.number().equals(card[1]))
 			    .findFirst()
                 .ifPresent(crd -> crd.position(0));
+            if (card[1].equals("wild") || card[1].equals("+4")) {
+                this.deck.stream()
+                    .filter(c -> c.position() == this.getPosition(id) && c.number().equals(card[1]))
+			        .findFirst()
+                    .ifPresent(crd -> crd.position(0));
+            }
             switch (card[1]) {
                 case "+2":
                     this.draw += 2;
@@ -263,7 +269,7 @@ public class Game {
 
     public void deal(String id, int amount) {
         for (int i = 0, j = 0; i < this.players.size(); i++) {
-            for (int k = 0; k < 7;) {
+            for (int k = 0; k < amount + this.draw;) {
                 Card card = this.deck.get(j);
                 if ((!card.color().equals("wild") && (card.number().equals("wild") || card.number().equals("+4"))) || card.position() == 1) {
                     j++;
@@ -273,6 +279,9 @@ public class Game {
                 j++;
                 k++;
             }
+        }
+        if (this.draw != 0) {
+            this.draw = 0;
         }
     }
 
